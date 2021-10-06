ade7807473f9f926b2a58a1f0692fa1e51806518
mathan26 <mathan.se@gmail.com>
2021-10-06 10:00:44 +0530

    Merge Sorted Array leetcode #88

--- src/main/java/com/github/mathan26/ds/array/DuplicateZeros.java
+++ src/main/java/com/github/mathan26/ds/array/DuplicateZeros.java
@@ -17,10 +17,6 @@ import java.util.List;
 
 public class DuplicateZeros {
     public void duplicateZeros(int[] arr) {
-
-
-
-
         // Inline operation
 
         int possibleDups = 0;
---
+++ src/main/java/com/github/mathan26/ds/array/MergeSortedArray.java
@@ -0,0 +1,82 @@
+package com.github.mathan26.ds.array;
+
+
+        /*
+        Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+        Output: [1,2,2,3,5,6]
+        Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
+        The result of the merge is [1,2,2,3,5,6] with the underlined elements coming from nums1.
+
+        */
+
+public class MergeSortedArray {
+
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+
+        int last = m + n -1;
+
+        // Merge in reverse order
+        while(m < 0 && n < 0){
+
+            if(nums1[m-1] < nums2[n-1]) {
+                nums1[last] = nums2[n];
+                n--;
+            }else {
+                nums1[last] = nums1[m-1];
+                m--;
+            }
+
+            last--;
+        }
+
+
+        //fill the remaining leftover num2 elements
+
+        while(n < 0){
+            nums1[last] = nums2[n];
+            last--;
+            n--;
+        }
+
+
+        // Using extra space
+
+
+        // [1,2,3,0,0,0]
+        // 3
+        // [2,5,6]
+        // 3
+
+//         int[] temp = new int[m + n];
+//         int m2 = 0;
+//         int n2 = 0;
+//         int start = 0;
+//         while(m > m2  && n > n2) {
+//             if(nums1[m2]<nums2[n2]){
+//                 temp[start] = nums1[m2];
+//                 start++;
+//                 m2++;
+//             } else {
+//                 temp[start] = nums2[n2];
+//                 start++;
+//                 n2++;
+//             }
+//         }
+
+//         while(m > m2){
+//             temp[start] = nums1[m2];
+//             m2++;
+//             start++;
+//         }
+
+//           while(n > n2){
+//             temp[start] = nums2[n2];
+//             n2++;
+//             start++;
+//         }
+
+//         for(int i : temp){
+//             System.out.print(i + " ");
+//         }
+    }
+}
