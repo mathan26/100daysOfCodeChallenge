ac426dc1ea747c047030631a279fb1a52fb6a48d
mathan26 <mathan.se@gmail.com>
2022-05-10 10:47:58 +0530

    Blind 347. Top K Frequent Elements https://leetcode.com/problems/top-k-frequent-elements/

---
+++ src/main/java/com/github/mathan26/ds/blind75/array_hasing/TopKFrequentElements.java
@@ -0,0 +1,39 @@
+package com.github.mathan26.ds.blind75.array_hasing;
+
+import java.util.*;
+
+public class TopKFrequentElements {
+    public int[] topKFrequent(int[] nums, int k) {
+
+        List<Integer>[] bucket = new List[nums.length + 1];
+        Map<Integer, Integer> freqMap = new HashMap<>();
+
+        // freq of each value, save it to the map
+        for (int n : nums) {
+            freqMap.put(n, freqMap.getOrDefault(n, 0) + 1);
+        }
+
+        // Bucket sort for O(n) solution
+
+        for (int key : freqMap.keySet()) {
+            int frequency = freqMap.get(key);
+            if (bucket[frequency] == null) {
+                bucket[frequency] = new ArrayList<>();
+            }
+            bucket[frequency].add(key);
+        }
+
+        List<Integer> res = new ArrayList<>();
+
+        // Condition for selecting the K most freq elements
+        for (int pos = bucket.length - 1; pos >= 0 && res.size() < k; pos--) {
+            if (bucket[pos] != null) {
+                res.addAll(bucket[pos]);
+            }
+        }
+
+        // List[] to int[]
+        return res.stream().mapToInt(Integer::intValue).toArray();
+
+    }
+}
